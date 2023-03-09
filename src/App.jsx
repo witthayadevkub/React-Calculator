3393bb03402af40480c071aa5bf896d8f8a208a3
witthayadevkub <tigermixdev@gmail.com>
2023-03-09 22:15:18 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,62 @@
+import React, { useState } from 'react';
+import "./App.css"
+
+function App() {
+  const [result, setResult] = useState('');
+
+  const handleClick = (e) => {
+    setResult(result.concat(e.target.name));
+  };
+
+  const clear = () => {
+    setResult('');
+  };
+
+  const backspace = () => {
+    setResult(result.slice(0, -1));
+  };
+
+  const calculate = () => {
+    try {
+      setResult(eval(result).toString());
+    } catch (error) {
+      setResult('Error');
+    }
+  };
+
+  return (
+    <div className="App">
+      <div className="calculator">
+            <input type="text" value={result} /> 
+        <div className="keypad"> 
+          <button className="clear" onClick={clear}>AC</button>
+          <button className="backspace" onClick={backspace}>C</button>
+          <button name="." onClick={handleClick}>.</button>
+          <button name="/" onClick={handleClick}>&divide;</button>      
+          <button name="7" onClick={handleClick}>7</button>
+          <button name="8" onClick={handleClick}>8</button>
+          <button name="9" onClick={handleClick}>9</button>
+          <button name="*" onClick={handleClick}>&times;</button>  
+          <button name="4" onClick={handleClick}>4</button>
+          <button name="5" onClick={handleClick}>5</button>
+          <button name="6" onClick={handleClick}>6</button>
+          <button name="-" onClick={handleClick}>&ndash;</button>  
+          <button name="1" onClick={handleClick}>1</button>
+          <button name="2" onClick={handleClick}>2</button>
+          <button name="3" onClick={handleClick}>3</button>
+          <button name="+" onClick={handleClick}>+</button>
+          <button name="0" onClick={handleClick}>0</button>
+          <button name="00" onClick={handleClick}>00</button>
+          <button name="000" onClick={handleClick}>000</button>
+          <button className="result" onClick={calculate}>=</button>   
+        </div>
+  
+        
+        </div>
+      </div>
+
+  );
+}
+
+export default App;
+
